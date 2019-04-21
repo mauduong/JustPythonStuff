cc66687cc2218c74192ce984a6e09845339c47ad
mauduong <mauduong723@gmail.com>
2019-04-21 19:47:42 +1200

    Different ways to extract cells based on Rows and Columns

---
+++ excelGetRowColumn.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+# excelGetRowColumn.py
+# mauduong
+# 21/04/2019
+
+import openpyxl
+
+workBook = openpyxl.load_workbook('example.xlsx')
+
+# Method 1
+sheet = workBook.get_sheet_by_name('Sheet1')
+tuple(sheet['A1':'C3'])
+
+for cellObjectRow in sheet['A1':'C3']:
+    for cellObjectColumn in cellObjectRow:
+        print(cellObjectColumn.coordinate, cellObjectColumn.value)
+    print('End of Row')
+
+
+# Method 2
+sheet = workBook.get_active_sheet()
+sheet.columns[1]
+for cellObjectRow in sheet.columns[1]:
+    print(cellObjectRow.value)
