071ef33a3d612b4a3490f14e7560aac5569c75f3
mauduong <mauduong723@gmail.com>
2019-04-22 23:35:14 +1200

    Compare cell values between two spreadsheets for duplicate values.

---
+++ compareExcelCells.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+# compareExcelRows.py
+# mauduong
+# 22/04/2019
+# Find duplicate cells between two excel spreadsheets.
+
+import openpyxl, time
+
+workBook1 = openpyxl.load_workbook('spreadsheet1.xlsx')
+sheet1 = workBook1.active
+workBook2 = openpyxl.load_workbook('spreadsheet2.xlsx')
+sheet2 = workBook2.active
+
+print('Searching...')
+startTime = time.time()
+rowNumber = 0
+numberOfDuplicates = 0
+for (column, column2) in zip(sheet1.iter_cols(), sheet2.iter_cols()):
+    for (cell, cell2) in zip(column, column2):
+        rowNumber += 1
+        if cell.value == cell2.value:
+            numberOfDuplicates += 1
+            print('Duplicate found.\nValue 1: ' + str(cell.value) + '\nValue 2: ' + str(cell2.value))
+            print('Cell: ' + str(cell.column) + str(cell.row) + '\nColumn: ' + str(cell.column) + ' Row: ' + str(cell.row) + '\n')
+endTime = time.time()
+print('Time taken: ' + str(round((endTime - startTime) * 1000, 2)) + 'ms')
+print('Duplicates found: ' + str(numberOfDuplicates))
