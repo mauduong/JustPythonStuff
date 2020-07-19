562369ea136b24f7aaaec379d316ce3064fb84d4
Michael Au-Duong <mauduong723@gmail.com>
2020-07-19 18:41:17 +1200

    For those moments when you're indecisive

---
+++ decideForMe.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+# decideForMe.py
+# mauduong
+# 19/07/2020
+
+import random
+
+print('Should I....')
+
+ans = random.randint(0,1)
+
+if (ans == 1):
+    print('Yes, just do it.')
+else:
+    print('No')
